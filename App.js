073c2faae81882484eabace802a226ee8a688190
Jonathan Engelsma <jengelsma@gmail.com>
2022-05-03 10:31:32 -0400

    Lecture 05 demo starts here.

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+
+import Calculator from "./screens/Calculator";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Calculator />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "stretch",
+    justifyContent: "flex-start",
   },
 });
---
+++ screens/Calculator.js
@@ -0,0 +1,109 @@
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+import React from "react";
+
+const Calculator = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.row}>
+        <Text style={styles.currentVal}> 0 </Text>
+      </View>
+      <View style={styles.row}>
+        <TouchableOpacity style={styles.modifierButton}>
+          <Text style={styles.text}>AC</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.modifierButton}>
+          <Text style={styles.text}>+/-</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.modifierButton}>
+          <Text style={styles.text}>%</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.operatorButton}>
+          <Text style={styles.text}>/</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.row}>
+        <TouchableOpacity style={styles.dataButton}>
+          <Text style={styles.text}>7</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.dataButton}>
+          <Text style={styles.text}>8</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.dataButton}>
+          <Text style={styles.text}>9</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.operatorButton}>
+          <Text style={styles.text}>X</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.row}>
+        <TouchableOpacity style={styles.dataButton}>
+          <Text style={styles.text}>4</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.dataButton}>
+          <Text style={styles.text}>5</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.dataButton}>
+          <Text style={styles.text}>6</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.operatorButton}>
+          <Text style={styles.text}>-</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.row}>
+        <TouchableOpacity style={styles.dataButton}>
+          <Text style={styles.text}>1</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.dataButton}>
+          <Text style={styles.text}>2</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.dataButton}>
+          <Text style={styles.text}>3</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.operatorButton}>
+          <Text style={styles.text}>+</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.row}>
+        <TouchableOpacity style={styles.dataButton}>
+          <Text style={styles.text}>0</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.dataButton}>
+          <Text style={styles.text}>.</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.operatorButton}>
+          <Text style={styles.text}>=</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "column", // default
+    justifyContent: "flex-start", //default
+  },
+  row: {},
+  currentVal: {},
+  modifierButton: {
+    backgroundColor: "#4D4B4B",
+  },
+  operatorButton: {
+    backgroundColor: "#FF9225",
+  },
+  dataButton: {
+    backgroundColor: "#696970",
+  },
+  text: {
+    color: "#fff",
+  },
+});
+
+export default Calculator;
